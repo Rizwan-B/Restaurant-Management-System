52bf92ad5a3aaadbbe93904a7af7d4718c27800f
Lucas <Lucas.kimber@outlook.com>
2022-01-24 18:17:04 +0000

    Added a toString() and toDiet() method in diet.

--- src/main/java/uk/ac/rhul/rms/Diet.java
+++ src/main/java/uk/ac/rhul/rms/Diet.java
@@ -7,22 +7,38 @@ package uk.ac.rhul.rms;
  *
  */
 public enum Diet {
-  
-  VEGAN("Ve"),
-  VEGETARIAN("V");
-  
+
+  VEGAN("Ve"), VEGETARIAN("V");
+
   private String dietSymbol;
-  
+
   Diet(String dietSymbol) {
     this.dietSymbol = dietSymbol;
   }
-  
+
   /**
-   * Returns a shorthand representation of the diet. 
+   * Returns a shorthand representation of the diet.
    *
    * @return The String representing the diet.
    */
   public String toString() {
     return this.dietSymbol;
+  }
+
+  /**
+   * Returns a Diet corrosponding to the string representation given. 
+   *
+   * @param diet the string representation of the diet.
+   * @return A diet corresponding to the string representation.
+   */
+  public static Diet toDiet(String diet) {
+    switch (diet) {
+      case "V":
+        return Diet.VEGETARIAN;
+      case "Ve":
+        return Diet.VEGAN;
+      default:
+        return null;
+    }
   }
 }
