967d27977da84480447814d5e1e3592613a9c2f2
Mohamed Yusuf <mohdgazyanii235@gmail.com>
2022-01-25 23:02:35 +0000

    Changed a bit of code written by Rizwan. The ArrayList will now hold entire menuItems instead of just integers to preven us from make extra queries to the DB later on.

--- src/main/java/uk/ac/rhul/rms/Order.java
+++ src/main/java/uk/ac/rhul/rms/Order.java
@@ -3,45 +3,49 @@ package uk.ac.rhul.rms;
 import java.util.ArrayList;
 
 /**
- * An object class that holds the entire order of the customer. 
+ * An object class that holds the entire order of the customer.
  *
  * @author Rizwan Bagdadi
  */
 public class Order {
-  
-  private ArrayList<Integer> menuItem = new ArrayList<>();
-  private int itemId;
-  
-  public Order(int itemId) {
-    this.itemId = itemId;
-  }
-  
-  public int getItemId() {
-    return itemId;
+
+  private ArrayList<MenuItem> orderList;
+
+  /**
+   * A public default constructor for the order object.
+   *
+   */
+  public Order() {
+    this.orderList = new ArrayList<MenuItem>();
   }
 
-  public ArrayList<Integer> getMenuItem() {
-    return menuItem;
+  /**
+   * A public getter method to return the customers order.
+   *
+   * @return The customers order as an ArrayList.
+   */
+  public ArrayList<MenuItem> getOrder() {
+    return this.orderList;
   }
 
   /**
    * This adds an item to the customers order.
    *
-   * @param itemId The id of the item to add.
+   * @param menuItem The menu Item to add to the order.
    */
-  public void addMenuItem(int itemId) {
-    menuItem.add(itemId); //Subject to change, to add from menu.
+  public void addMenuItem(MenuItem menuItem) {
+    this.orderList.add(menuItem);
   }
-  
+
   /**
    * This removes the item from the customers order.
    *
-   * @param itemId The id of the item.
+   * @param menuItemId The id of the item to remove.
    */
-  public void removeMenuItem(int itemId) {
-    for (int i = 0; i < menuItem.size(); i++) {
-      if (itemId == menuItem.get(i)) {
-        menuItem.remove(itemId);
+  public void removeMenuItem(int menuItemId) {
+    for (int i = 0; i < this.orderList.size(); i++) {
+      if (this.orderList.get(i).getId() == menuItemId) {
+        this.orderList.remove(i);
       }
     }
   }
