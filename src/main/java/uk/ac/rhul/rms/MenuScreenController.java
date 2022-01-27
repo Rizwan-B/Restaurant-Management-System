1b5392e906b202e423ff0c5dd80c1ac817f73b31
Lucas <Lucas.kimber@outlook.com>
2022-01-27 23:52:27 +0000

    Edited MenuScreen and its controller, along with StartScreenController, in preperation for connecting frontend to database.

--- src/main/java/uk/ac/rhul/rms/MenuScreenController.java
+++ src/main/java/uk/ac/rhul/rms/MenuScreenController.java
@@ -1,5 +1,8 @@
 package uk.ac.rhul.rms;
 
+import java.sql.Connection;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import uk.ac.rhul.screenmanager.ControlledScreen;
 import uk.ac.rhul.screenmanager.ScreensController;
 
@@ -12,11 +15,19 @@ import uk.ac.rhul.screenmanager.ScreensController;
 public class MenuScreenController implements ControlledScreen {
 
   ScreensController screensController;
+  private Connection connection;
   
   @Override
   public void setScreenParent(ScreensController screenParent) {
     this.screensController = screenParent;
+    this.connection = DatabaseController.connection();
   }
   
+  @FXML
+  private ListView<String> starterList = new ListView<String>();
   
+  @FXML
+  public void addItems() {
+    starterList.getItems().add("Hey");
+  }
 }
