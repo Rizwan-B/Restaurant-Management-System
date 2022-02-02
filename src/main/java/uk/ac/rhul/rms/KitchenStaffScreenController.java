d5cfbd2ffdae1efe47ace806a9543efce85ec70b
Lucas <Lucas.kimber@outlook.com>
2022-02-02 17:09:05 +0000

    Added screen and screen controller for the Kitchen Staff side of the application in preparation for work on the Kitchen Staff user stories.

---
+++ src/main/java/uk/ac/rhul/rms/KitchenStaffScreenController.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.rms;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class KitchenStaffScreenController {
+
+    @FXML
+    private ListView<?> pendingOrders;
+
+    @FXML
+    private ListView<?> workingOn;
+
+}
