21115a5958a3c984e9b9ef11359f8bb5cccfdc10
Lucas <Lucas.kimber@outlook.com>
2022-02-07 16:54:07 +0000

    Created an exception for when an attempt is made to get a non-existent menu item from the database.

---
+++ src/main/java/uk/ac/rhul/rms/InvalidMenuIdException.java
@@ -0,0 +1,19 @@
+package uk.ac.rhul.rms;
+
+/**
+ * An exception that is thrown when an attempt is made to get a menu item using a non-existent primary key.
+ */
+public class InvalidMenuIdException extends Exception {
+
+  /**
+   * The serialVersionUID for Java Exceptions.
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Sends the relevant exception message to the exception super class.
+   */
+  public InvalidMenuIdException() {
+    super("An attempt has been made to get a MenuItem with an ID that does not exist in the database.");
+  }
+}
