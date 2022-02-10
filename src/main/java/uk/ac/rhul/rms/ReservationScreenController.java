0b3af3e66451c255600fa11dd3d64cdf488a27d4
muqdas123 <zhac334@live.rhul.ac.uk>
2022-02-10 15:41:13 +0000

    Added multiple functions in ReservationScreen.fxml:
    1) Added textfield for name and table Number
    2) Added a reserve button

--- src/main/java/uk/ac/rhul/rms/ReservationScreenController.java
+++ src/main/java/uk/ac/rhul/rms/ReservationScreenController.java
@@ -5,8 +5,8 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
 import uk.ac.rhul.screenmanager.ControlledScreen;
 import uk.ac.rhul.screenmanager.ScreensController;
 
@@ -48,6 +48,27 @@ public class ReservationScreenController implements ControlledScreen, Initializa
     @FXML
     private ListView<Integer> table_no;
 
+    @FXML
+    private ComboBox<String> timePicker;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Text tNumber;
+
+    @FXML
+    private TextField tableInput;
+
+    @FXML
+    private Text fullName;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private Button reserve;
+
 
     public void tableSeat() {
         try {
@@ -70,6 +91,11 @@ public class ReservationScreenController implements ControlledScreen, Initializa
     public void initialize(URL location, ResourceBundle resources) {
         this.connection = DatabaseConnection.getInstance();
         tableSeat();
+        ObservableList<String> list = FXCollections.observableArrayList("12:00",
+                                                                                "1:00", "2:00", "3:00","4:00","5:00",
+                                                                                "6:00", "7:00","8:00",
+                                                                                "9:00", "10:00");
+        timePicker.setItems(list);
 
 
     }
