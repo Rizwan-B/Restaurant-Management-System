2ac7e71bc349f3c2a5b2c01d3e54d3b5e7f545d9
virginialitta <zjac135@live.rhul.ac.uk>
2022-02-15 17:57:18 +0100

    Added a confirmation alert for order cancelling.

--- src/main/java/uk/ac/rhul/rms/ManageOrderScreenController.java
+++ src/main/java/uk/ac/rhul/rms/ManageOrderScreenController.java
@@ -4,7 +4,9 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import uk.ac.rhul.screenmanager.ControlledScreen;
 import uk.ac.rhul.screenmanager.ScreensController;
@@ -54,15 +56,21 @@ public class ManageOrderScreenController implements ControlledScreen, Initializa
     @FXML
     void cancelBtnPressed(ActionEvent event) {
 
-        try {
-            ObservableList selectedItem = orderList.getSelectionModel().getSelectedIndices();
-            int orderId = Integer.parseInt(orderList.getItems().get(convertToInt(selectedItem.toString())).split("|")[0].trim());
+        Alert alert = new Alert(Alert.AlertType.NONE, "Are you sure you want to cancel this order?", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
 
-            DatabaseController.cancelOrder(DatabaseConnection.getInstance(), orderId);
-            this.orderList.getItems().clear();
-            this.displayItems();
-        } catch (Exception e) {
-            System.out.println("ERROR: SQL connection error, or you did not select an item to delete.");
+        if (alert.getResult() == ButtonType.YES) {
+
+            try {
+                ObservableList selectedItem = orderList.getSelectionModel().getSelectedIndices();
+                int orderId = Integer.parseInt(orderList.getItems().get(convertToInt(selectedItem.toString())).split("|")[0].trim());
+
+                DatabaseController.cancelOrder(DatabaseConnection.getInstance(), orderId);
+                this.orderList.getItems().clear();
+                this.displayItems();
+            } catch (Exception e) {
+                System.out.println("ERROR: SQL connection error, or you did not select an item to delete.");
+            }
         }
 
     }
