a8069d820940a0d49d791fb137318f7ab04310c7
Jacob Artis <zjac219@live.rhul.ac.uk>
2022-02-17 16:03:04 +0000

    Added a basket listView and a button that adds the selected menu items to the basket

--- src/main/java/uk/ac/rhul/rms/MenuScreenController.java
+++ src/main/java/uk/ac/rhul/rms/MenuScreenController.java
@@ -41,6 +41,9 @@ public class MenuScreenController implements ControlledScreen, Initializable {
     private ListView<String> dessertList;
 
     @FXML
+    private ListView<String> basketList;
+
+    @FXML
     private Button backBtn;
 
     @FXML
@@ -203,7 +206,19 @@ public class MenuScreenController implements ControlledScreen, Initializable {
         }
     }
 
+    @FXML
+    void add(ActionEvent event){
+        if (starterList.getSelectionModel().getSelectedItem()!= null){
+            basketList.getItems().add(starterList.getSelectionModel().getSelectedItem());
+        }
+        if (mainList.getSelectionModel().getSelectedItem()!= null){
+            basketList.getItems().add(mainList.getSelectionModel().getSelectedItem());
+        }
+        if (dessertList.getSelectionModel().getSelectedItem()!= null){
+            basketList.getItems().add(dessertList.getSelectionModel().getSelectedItem());
+        }
 
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -211,4 +226,6 @@ public class MenuScreenController implements ControlledScreen, Initializable {
         ObservableList<String> list = FXCollections.observableArrayList("Non-Vegetarian", "Vegetarian", "Vegan");
         filterBox.setItems(list); // This initialises the drop-down menu with 3 diet options.
     }
+
+
 }
