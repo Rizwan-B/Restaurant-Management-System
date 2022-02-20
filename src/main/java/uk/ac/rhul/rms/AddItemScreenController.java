6a04e4deffc6d5fd2f12f54c58d2611e463faa0a
muqdas123 <zhac334@live.rhul.ac.uk>
2022-02-20 00:02:34 +0000

    Designing Add Item Screen with buttons and fields

--- src/main/java/uk/ac/rhul/rms/AddItemScreenController.java
+++ src/main/java/uk/ac/rhul/rms/AddItemScreenController.java
@@ -3,6 +3,8 @@ package uk.ac.rhul.rms;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import uk.ac.rhul.screenmanager.ControlledScreen;
 import uk.ac.rhul.screenmanager.ScreensController;
@@ -16,10 +18,52 @@ public class AddItemScreenController implements ControlledScreen {
     }
 
     @FXML
+    private Button addItem;
+
+    @FXML
+    private Text ChooseDiet;
+
+    @FXML
     private Button backButton;
 
     @FXML
+    private TextField calories;
+
+    @FXML
+    private Text chooseOne;
+
+    @FXML
+    private CheckBox dessert;
+
+    @FXML
     private Text headLine;
+
+    @FXML
+    private TextField itemDescription;
+
+    @FXML
+    private TextField itemID;
+
+    @FXML
+    private TextField itemImage;
+
+    @FXML
+    private TextField itemName;
+
+    @FXML
+    private CheckBox main;
+
+    @FXML
+    private CheckBox nonVeg;
+
+    @FXML
+    private CheckBox starter;
+
+    @FXML
+    private CheckBox vegan;
+
+    @FXML
+    private CheckBox vegetarian;
 
     @FXML
     void backBtnPressed(ActionEvent event) {
