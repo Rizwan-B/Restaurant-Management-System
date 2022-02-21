26de5ecb8036d58901c755b276ab4a93bc8ac679
muqdas123 <zhac334@live.rhul.ac.uk>
2022-02-21 21:42:28 +0000

    Adding values on date and year ComboBox

--- src/main/java/uk/ac/rhul/rms/BasketScreenController.java
+++ src/main/java/uk/ac/rhul/rms/BasketScreenController.java
@@ -1,5 +1,8 @@
 package uk.ac.rhul.rms;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import uk.ac.rhul.screenmanager.ControlledScreen;
@@ -8,7 +11,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javax.swing.*;
+import java.net.URL;
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 /**
  * The screen controller for the basket screen implementing the ControlledScreen Interface.
@@ -16,9 +22,10 @@ import java.util.ArrayList;
  * @author Jacob Artis
  *  
  */
-public class BasketScreenController implements ControlledScreen {
+public class BasketScreenController implements ControlledScreen, Initializable {
 
   ScreensController screensController;
+  private Connection connection;
   
   @Override
   public void setScreenParent(ScreensController screenParent) {
@@ -44,7 +51,7 @@ public class BasketScreenController implements ControlledScreen {
   private Text dash;
 
   @FXML
-  private ComboBox<Integer> date;
+  private ComboBox<String> date;
 
   @FXML
   private CheckBox electronicPayment;
@@ -77,12 +84,27 @@ public class BasketScreenController implements ControlledScreen {
   private Text userName;
 
   @FXML
-  private ComboBox<Integer> year;
+  private ComboBox<String> year;
 
   @FXML
   void backBtnPressed(ActionEvent event) {
     this.screensController.loadScreen(Main.menuScreenID, Main.menuScreenFile);
     this.screensController.setScreen(Main.menuScreenID);
+  }
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    this.connection = DatabaseConnection.getInstance();
+
+    ObservableList<String> list = FXCollections.observableArrayList("1", "2", "3","4","5",
+            "6", "7","8", "9", "10","11","12","13","14","15","16","17","18","19","20","21","22","23","24",
+            "25","26","27","28","29","30","31");
+    date.setItems(list);
+    ObservableList<String> listYear = FXCollections.observableArrayList("2022", "2023", "2024","2025",
+            "2026", "2027","2028", "2029", "2030","2031","2032","2033","2034","2035","2036",
+            "2037","2038","2039","2040");
+    year.setItems(listYear);
+
+
   }
 
 }
